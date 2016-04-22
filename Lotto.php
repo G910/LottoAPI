ba693029a691203ed09cffcc6fb2821ab33c2300
G910 <beniaminkowol@gmail.com>
2016-04-22 14:50:45 +0200

    Change API address

--- Lotto.php
+++ Lotto.php
@@ -2,7 +2,7 @@
 
 Class Lotto{
 	
-	private $_APIAddress = "http://localhost/lotto/API.php";
+	private $_APIAddress = "http://mrgod.pl/lotto/API.php";
 	private $_clientID = null;
 	private $_clientSecret = null;
 	private $_connected = false;
