04de34338fe6f3716f62f421e0c88dddd939eeae
G910 <beniaminkowol@gmail.com>
2016-04-23 13:51:14 +0200

    API upgrades

--- Lotto.php
+++ Lotto.php
@@ -2,173 +2,168 @@
 
 Class Lotto{
 	
-	private $_APIAddress = "http://81.2.251.149/lotto/API.php";
-	private $_clientID = null;
-	private $_clientSecret = null;
-	private $_connected = false;
-	private $_lastErrorCode;
-	private $_lastErrorDesc;
-	private $_time;
-	private $_token;
-	private $_debug = false;
+	private $APIAddress = "http://81.2.251.149/lotto/API.php";
+	private $clientID = null;
+	private $clientSecret = null;
+	private $connected = false;
+	private $lastErrorCode;
+	private $lastErrorDesc;
+	private $time;
+	private $token;
+	private $debug = false;
+	private $debugFile = "./LottoAPI.log";
 	
 	public function __construct($clientID = null, $clientSecret = null, $debug = false){
-		$this->_clientID = $clientID;
-		$this->_clientSecret = $clientSecret;
-		$this->_time = time();
+		$this->clientID = $clientID;
+		$this->clientSecret = $clientSecret;
+		$this->time = time();
 		$this->connect();
-		$this->_debug = $debug;
+		$this->debug = $debug;
+	}
+	
+	private function appendLog($content){
+		if(!file_exists($this->debugFile))
+			file_put_contents($this->debugFile, "");
+		file_put_contents($this->debugFile, file_get_contents($this->debugFile).$content);
 	}
 	
 	private function curl($postData){
 		$postData['time'] = time();
-		$postData['token'] = $this->_token;
+		$postData['token'] = $this->token;
 		$postData['clientIP'] = $_SERVER['REMOTE_ADDR'];
 		$postDataJSON = json_encode($postData);
 		$postDataJSONArr['request'] = $postDataJSON;
-		$ch = curl_init($this->_APIAddress);
+		$ch = curl_init($this->APIAddress);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $postDataJSONArr);
 		$response = curl_exec($ch);
-		if($this->_debug){
-			echo "<br>Request:".$postDataJSON."<br>";
-			echo "Response: ".$response."<br>";
+		if($this->debug){
+			$this->appendLog("Request:".$postDataJSON."\r\nResponse: ".$response."\r\n\r\n");
 		}
 		curl_close($ch);
-		return json_decode($response, true);
+		$response = json_decode($response, true);
+		if(empty($response['status']) || $response['status'] != 1) {
+			$this->lastErrorCode = $response['error']['code'];
+			$this->lastErrorDesc = $response['error']['description'];
+			return false;
+		}
+		return $response;
+	}
+	
+	private function isDateValid($date){
+		$isCorrectData = preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date);
+		if ($date !== null && !$isCorrectData) {
+			$this->lastErrorCode = "clientSide1";
+			$this->lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
+			return false;
+		}
+		return $date;
 	}
 	
 	public function connect(){
-		$data['id'] = $this->_clientID;
-		$data['secret'] = $this->_clientSecret;
-		$data['constructTime'] = $this->_time;
+		$data['id'] = $this->clientID;
+		$data['secret'] = $this->clientSecret;
+		$data['constructTime'] = $this->time;
 		$data['type'] = "authenticate";
 		$data['clientIP'] = $_SERVER['REMOTE_ADDR'];
 		$response = $this->curl($data);
-		if(isset($response['status']) && $response['status'] == 1){
-			$this->_connected = true;
-			$this->_token = $response['token'];
-			return true;
-		}else{
-			$this->_lastErrorCode = $response['error']['code'];
-			$this->_lastErrorDesc = $response['error']['description'];
-			return false;
-		}
+	 	$this->connected = true;
+		$this->token = $response['token'];
+		return true;
 	}
 	
 	public function getLastError(){
-		$error = (!empty($this->_lastErrorCode)) ? "[#ERR ".$this->_lastErrorCode."]: ".$this->_lastErrorDesc: null;
+		$error = (!empty($this->lastErrorCode)) ? "[#ERR ".$this->lastErrorCode."]: ".$this->lastErrorDesc: false;
 		return $error;
 	}
 	
-	public function getLotto($date = 0){
-		if($date == 0){
-			$data['type'] = "getLotto";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getLottoByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getLotto($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getLotto';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getLottoPlus($date = 0){
-		if($date == 0){
-			$data['type'] = "getLottoPlus";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getLottoPlusByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getLottoPlus($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getLottoPlus';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getMiniLotto($date = 0){
-		if($date == 0){
-			$data['type'] = "getMiniLotto";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getMiniLottoByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getMiniLotto($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getMiniLotto';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getMultiMulti($date = 0){
-		if($date == 0){
-			$data['type'] = "getMultiMulti";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getMultiMultiByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getMultiMulti($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getMultiMulti';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getKaskada($date = 0){
-		if($date == 0){
-			$data['type'] = "getKaskada";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getKaskadaByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getKaskada($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getKaskada';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getEkstraPensja($date = 0){
-		if($date == 0){
-			$data['type'] = "getEkstraPensja";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getEkstraPensjaByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getEkstraPensja($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getEkstraPensja';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
 	
-	public function getKenyo($date = 0){
-		if($date == 0){
-			$data['type'] = "getKenyo";
-		}else{
-			if(preg_match("|[0-9]{4}-[0-9]{2}-[0-9]{2}|", $date)){
-				$data['type'] = "getKenyoByDate";
-				$data['date'] = $date;
-			}else{
-				$this->_lastErrorCode = "clientSide1";
-				$this->_lastErrorDesc = "Data wprowadzona w niewłaściwym formacie. Prawidłowy: YYYY-MM-DD";
-				return false;
-			}
+	public function getKenyo($date = null){
+		if(($date = $this->isDateValid($date)) === false) {
+			return false;
+		}
+		$data = [];
+		$data['type'] = 'getKenyo';
+		if ($date !== null) {
+			$data['type'] .= 'ByDate';
+			$data['date'] = $date;
 		}
 		return $this->curl($data);
 	}
